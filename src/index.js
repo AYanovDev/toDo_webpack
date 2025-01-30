f9f4b43c5df4acf60decd0eab9db24e7d719cd53
AYanovDev <alex.ianovskii@mail.ru>
2025-01-30 21:53:19 +0500

    fixed memory-related code

--- src/index.js
+++ src/index.js
@@ -9,17 +9,50 @@ class ToDo {
     this.prioriity = prioriity;
   }
 }
-const projects = [new Project("default")];
-// localStorage.setItem("projects", JSON.stringify(projects));
-const list = new List(JSON.parse(localStorage.getItem("projects")));
+
+function getProjects() {
+  if (localStorage.getItem("projects")) {
+    const projects = JSON.parse(localStorage.getItem("projects"));
+    console.log(projects);
+    let renderableProjects = projects.map((project) => {
+      return new Project(project.name);
+    });
+    return renderableProjects;
+  } else {
+    const projects = [new Project("default")];
+    return projects;
+  }
+}
+
+const projects = getProjects();
+const list = new List(projects); // create an if statement to verify that the memory is not empty
 list.render();
+
+// if (localStorage.getItem("projects")) {
+//   const projects = JSON.parse(localStorage.getItem("projects"));
+//   console.log(projects);
+//   let renderableProjects = projects.map((project) => {
+//     return new Project(project.name);
+//   });
+//   const list = new List(renderableProjects); // create an if statement to verify that the memory is not empty
+//   list.render();
+// } else {
+//   const projects = [new Project("default")];
+//   const list = new List(projects); // create an if statement to verify that the memory is not empty
+//   list.render();
+//   localStorage.setItem("projects", JSON.stringify(projects));
+// }
+// const projects = JSON.parse(localStorage.getItem("projects"));
+// // const projects = [new Project("default")];
+// // localStorage.setItem("projects", JSON.stringify(projects));
+// const list = new List(projects); // create an if statement to verify that the memory is not empty
+// list.render();
 
 let addProject = document.querySelector(".add_project");
 addProject.addEventListener("click", () => {
   let newProject = new Project(prompt("What's your next project?: "));
   projects.push(newProject);
-  localStorage.removeItem("projects");
   localStorage.setItem("projects", JSON.stringify(projects));
-  list.projects = projects;
+  //   list.projects = projects; // added by chatGPT
   list.render();
 });
--- src/project.js
+++ src/project.js
@@ -1,6 +1,7 @@
 export class Project {
-  constructor(name) {
+  constructor(name, to_dos = []) {
     this.name = name;
+    this.to_dos = to_dos;
   }
   renderContent() {
     document.querySelector(".contents").textContent = this.name;
