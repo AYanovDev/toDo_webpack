639cc45895a593c0b3aafc7736edac58c4faa1ce
AYanovDev <alex.ianovskii@mail.ru>
2025-02-13 22:07:01 +0500

    Completed data storage of projects and to-dos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import { List } from "./list";
 import { Project } from "./project";
 import "./template.css";
-class ToDo {
-  constructor(title, description, dueDate, prioriity) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.prioriity = prioriity;
-  }
-}
 
 function getProjects() {
   if (localStorage.getItem("projects")) {
@@ -25,34 +17,18 @@ function getProjects() {
 }
 
 const projects = getProjects();
-const list = new List(projects); // create an if statement to verify that the memory is not empty
+const list = new List(projects);
 list.render();
-
-// if (localStorage.getItem("projects")) {
-//   const projects = JSON.parse(localStorage.getItem("projects"));
-//   console.log(projects);
-//   let renderableProjects = projects.map((project) => {
-//     return new Project(project.name);
-//   });
-//   const list = new List(renderableProjects); // create an if statement to verify that the memory is not empty
-//   list.render();
-// } else {
-//   const projects = [new Project("default")];
-//   const list = new List(projects); // create an if statement to verify that the memory is not empty
-//   list.render();
-//   localStorage.setItem("projects", JSON.stringify(projects));
-// }
-// const projects = JSON.parse(localStorage.getItem("projects"));
-// // const projects = [new Project("default")];
-// // localStorage.setItem("projects", JSON.stringify(projects));
-// const list = new List(projects); // create an if statement to verify that the memory is not empty
-// list.render();
 
 let addProject = document.querySelector(".add_project");
 addProject.addEventListener("click", () => {
   let newProject = new Project(prompt("What's your next project?: "));
   projects.push(newProject);
   localStorage.setItem("projects", JSON.stringify(projects));
-  //   list.projects = projects; // added by chatGPT
   list.render();
 });
+
+// last selected project needs to stay selected
+// Default must be selected if none are
+// styling
+// removing projects/to-do's
--- src/project.js
+++ src/project.js
@@ -1,9 +1,55 @@
 export class Project {
-  constructor(name, to_dos = []) {
+  #to_dos;
+
+  constructor(name) {
     this.name = name;
-    this.to_dos = to_dos;
+    this.#to_dos = this.#getTodos();
   }
+
+  #renderTodos() {
+    let renderableTodos = this.#to_dos.map((to_do) => {
+      let renderedTodo = document.createElement("p");
+      renderedTodo.textContent = to_do;
+      return renderedTodo;
+    });
+    let content_div = document.querySelector(".contents");
+    content_div.append(...renderableTodos);
+  }
+
+  #getTodos() {
+    if (localStorage.getItem("todo" + this.name)) {
+      let romTodos = JSON.parse(localStorage.getItem("todo" + this.name));
+      return romTodos;
+    } else {
+      let romTodos = ["Here go the things you need to do."];
+      return romTodos;
+    }
+  }
+
   renderContent() {
-    document.querySelector(".contents").textContent = this.name;
+    // add new to-do section
+    let add_todo = document.createElement("button");
+    add_todo.textContent = "Add a to-do to the list";
+    let content_div = document.querySelector(".contents");
+    content_div.innerHTML = "";
+    content_div.append(add_todo);
+
+    add_todo.addEventListener("click", () => {
+      let new_todo = prompt("Write what needs to be done here: ");
+      let renderedTodo = document.createElement("p");
+      renderedTodo.textContent = new_todo;
+      content_div.append(renderedTodo);
+      this.#to_dos.push(new_todo);
+      localStorage.setItem("todo" + this.name, JSON.stringify(this.#to_dos));
+    });
+
+    this.#renderTodos();
+
+    // load to-dos from memory section
+    // let memTodos = this.#getTodos();
+    // memTodos.map((item) => {
+    //   content_div.append(item);
+    // });
+    // document.querySelector(".contents").textContent = this.name;
   }
 }
